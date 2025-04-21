1e161c93ccc726724cee6d7c44b100841f1dfe79
zaid shaikh <zaid@prismitworks.com>
2025-04-21 18:21:24 +0530

    added changes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    tsconfigRaw: require('./tsconfig.json')  // or './tsconfig.ignore.json' if you make a separate one
+  }
+});
